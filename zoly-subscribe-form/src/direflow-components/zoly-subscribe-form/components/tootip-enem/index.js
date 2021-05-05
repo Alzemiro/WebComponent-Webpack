43d8d84257b5dc689f16884fbe1780d0688905ff
Alzemiro Iago de Moraes Thomaz <iagomoraest@gmail.com>
2021-05-05 15:10:48 -0300

    Tooltip

--- zoly-subscribe-form/src/direflow-components/zoly-subscribe-form/components/tootip-enem/index.js
+++ zoly-subscribe-form/src/direflow-components/zoly-subscribe-form/components/tootip-enem/index.js
@@ -1,38 +1,38 @@
 import React from "react";
-import { Modal, Backdrop, Fade, Typography } from "@material-ui/core";
-import useStyles from "./tootltipEnem.style";
+import { Backdrop, Fade, Typography, Dialog } from "@material-ui/core"
+import { Styled } from "direflow-component";
+import styles from './tooltip-enem-component.style.css'
 
 // eslint-disable-next-line react/prop-types
 const TooltipEnem = ({ open, onClose }) => {
-  const classes = useStyles();
   return (
-     <Modal
-      aria-labelledby="transition-modal-title"
-      aria-describedby="transition-modal-description"
-      className={classes.modal}
-      open={open}
-      onClose={onClose}
-      closeAfterTransition
-      BackdropComponent={Backdrop}
-      BackdropProps={{
+    <Styled styles={styles}>
+      <Dialog
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className="modal"
+        open={open}
+        onClose={onClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
           timeout: 300,
         }}
         >
-      <Fade in={open}>
-        <div className={classes.paper}>
-          <Typography className={classes.title}>
-            Ei, você Sabia Disso?
-          </Typography>
-          <Typography className={classes.text}>
-            Você pode usar sua <strong>nota do Enem</strong> se fez a prova nos{" "}
-            <strong>últimos dez anos</strong>. Se você optar pela inscrição
-            através da nota do Enem,{" "}
-            <strong>não precisará realizar o vestibular tradicional.</strong>
-          </Typography>
-        </div>
-      </Fade>
-    </Modal> 
-  )
-}
+        <Fade in={open}>
+          <div className="paper">
+            <Typography className="title">Ei, você Sabia Disso?</Typography>
+            <Typography className="text">
+              Você pode usar sua <strong>nota do Enem</strong> se fez a prova
+              nos <strong>últimos dez anos</strong>. Se você optar pela
+              inscrição através da nota do Enem,{" "}
+              <strong>não precisará realizar o vestibular tradicional.</strong>
+            </Typography>
+          </div>
+        </Fade>
+      </Dialog>
+    </Styled>
+  );
+};
 
 export default TooltipEnem
