c0ef70628c432753d903ef0718e0c6993725d2c5
Alzemiro Iago de Moraes Thomaz <iagomoraest@gmail.com>
2021-05-07 09:36:40 -0300

    Teste timer

--- zoly-subscribe-form/src/direflow-components/zoly-subscribe-form/components/tooltip-gatilho/index.js
+++ zoly-subscribe-form/src/direflow-components/zoly-subscribe-form/components/tooltip-gatilho/index.js
@@ -13,32 +13,34 @@ const TooltipGatilho = () => {
       <div className="modal__gatilho display-block">
         <div className="paper">
           <div className="modal__text">
-            <div className="modal__text title">
-              Ei, não perca tempo!
-            </div>
+            <div className="modal__text title">Ei, não perca tempo!</div>
             <div className="modal__text text">
               Essa oferta vai durar apenas:
             </div>
           </div>
-          <div className="timer__around">
-            <div className="time__img">
-            <img src={"https://storage.googleapis.com/cro_seo-hospedagem_de_assets/Imagens%20Kroton%20-%20Ex%20Maxymiser/anhanguera/SVGS/ampulheta.svg"}/>
+        <div className="timer__around">
+          <div className="timer__container">
+              <img
+                  src={
+                    "https://storage.googleapis.com/cro_seo-hospedagem_de_assets/Imagens%20Kroton%20-%20Ex%20Maxymiser/anhanguera/SVGS/ampulheta.svg"
+                  }
+                />
+                <Timer
+                  initialTime={900000}
+                  direction="backward"
+                  formatValue={(value) => `${value < 10 ? `0${value}` : value}`}
+                >
+                  {() => (
+                    <React.Fragment>
+                      <Timer.Minutes />:
+                      <Timer.Seconds />
+                    </React.Fragment>
+                  )}
+                </Timer>
+              </div>
             </div>
-            <Timer
-              initialTime={900000}
-              direction="backward"
-              formatValue={(value) => `${value < 10 ? `0${value}` : value}`}
-            >
-              {() => (
-                <React.Fragment>
-                  <Timer.Minutes />:
-                  <Timer.Seconds />
-                </React.Fragment>
-              )}
-            </Timer>
           </div>
         </div>
-      </div>
     </Styled>
   );
 };
